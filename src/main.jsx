e736f2b98848ed90105efd09ddee234a460c1bcf
fahad-aslam8743 <notxfahad@gmail.com>
2026-03-23 22:56:48 +0500

    Fix : Case-Error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { CartProvider } from './Context/CartContext';
+import { CartProvider } from './context/CartContext';
 import App from './App';
 import './index.css';
 const queryClient = new QueryClient();
